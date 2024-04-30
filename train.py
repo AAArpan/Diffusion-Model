0f16b1dc59d765c210953f979291770ad336d94a
Arpan Srivastava <108794407+AAArpan@users.noreply.github.com>
2024-04-30 18:18:13 +0530

    Update train.py
    Added img_size and crop_size

--- train.py
+++ train.py
@@ -14,61 +14,51 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 def train(args):
-    # Read the config file #
     with open(args.config_path, 'r') as file:
         try:
             config = yaml.safe_load(file)
         except yaml.YAMLError as exc:
             print(exc)
     print(config)
-    ########################
     
     diffusion_config = config['diffusion_params']
     dataset_config = config['dataset_params']
     model_config = config['model_params']
     train_config = config['train_params']
+    img_size = config['im_size']
+    crop_size = config['crop_size']
     
-    # Create the noise scheduler
     scheduler = LinearNoiseScheduler(num_timesteps=diffusion_config['num_timesteps'],
                                      beta_start=diffusion_config['beta_start'],
                                      beta_end=diffusion_config['beta_end'])
     
-    # Create the dataset
-    data = CustomData('train', im_path=dataset_config['im_path'])
-    data_loader = DataLoader(data, batch_size=train_config['batch_size'], shuffle=True, num_workers=4)
+    data = CustomData('train', im_path=dataset_config['im_path'], img_size =img_size, crop_size = crop_size)
+    data_loader = DataLoader(data, batch_size=train_config['batch_size'], shuffle=True, num_workers=2)
     
-    # Instantiate the model
     model = Unet(model_config).to(device)
     model.train()
     
-    # Create output directories
     if not os.path.exists(train_config['task_name']):
         os.mkdir(train_config['task_name'])
     
-    # Load checkpoint if found
     if os.path.exists(os.path.join(train_config['task_name'],train_config['ckpt_name'])):
         print('Loading checkpoint as found one')
         model.load_state_dict(torch.load(os.path.join(train_config['task_name'],
                                                       train_config['ckpt_name']), map_location=device))
-    # Specify training parameters
     num_epochs = train_config['num_epochs']
     optimizer = Adam(model.parameters(), lr=train_config['lr'])
     criterion = torch.nn.MSELoss()
     
-    # Run training
     for epoch_idx in range(num_epochs):
         losses = []
         for im in tqdm(data_loader):
             optimizer.zero_grad()
             im = im.float().to(device)
-            
-            # Sample random noise
+
             noise = torch.randn_like(im).to(device)
-            
-            # Sample timestep
+
             t = torch.randint(0, diffusion_config['num_timesteps'], (im.shape[0],)).to(device)
             
-            # Add noise to images according to timestep
             noisy_im = scheduler.add_noise(im, noise, t)
             noise_pred = model(noisy_im, t)
 
